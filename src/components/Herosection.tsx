6a4e066850eb9bf806c7e3fb557f7bc8122b9811
Megha Varshini <Varshinim173@gmail.com>
2025-03-28 15:43:02 +0530

    Update Herosection.tsx

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Herosection = () => {
   return (
     // Hero Section
-    <div id="#cta" className="bg-black relative overflow-hidden">
+    <div id="#cta" className="bg-black relative overflow-visible">
       {/* Wrapping Div */}
       <div className="container text-white flex  overflow-clip flex-col justify-center items-center mx-auto">
         {/* Glowing Ring */}
-        <div className="absolute top-[150px] md:top-8  rounded-full glowing-ring opacity-40  h-[280px] w-[400px] md:h-[350px] md:w-[500px] lg:h-[400px] lg:w-[510px]  border-[32px] border-[#8a2be2]"></div>
+        <div className="absolute top-[150px] md:top-8  rounded-full glowing-ring opacity-60 mix-blend-screen  h-[280px] w-[400px] md:h-[350px] md:w-[500px] lg:h-[400px] lg:w-[510px]  border-[32px] border-[#8a2be2]"></div>
         {/* Content */}
         <div className="relative flex flex-col justify-center items-center pb-59 sm:mb-30 md:pb-0">
           <div className="text-4xl sm:text-[44px] md:text-[48px] lg:text-[55px] font-poppins mt-[190px] md:mt-[40px] text-center top-[210px] ">
