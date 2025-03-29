c39f859cb662232308238b794c2a7fa318cb2104
Megha Varshini <Varshinim173@gmail.com>
2025-03-29 18:04:20 +0530

    Update Herosection.tsx

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -26,7 +26,7 @@ import Link from "next/link";
          </div>
        </div>
        {/* Linear Gradient at bottom */}
-       <div className="h-[100px] mb-[0px] w-full  bg-[linear-gradient(to_bottom,transparent,#52057B,#52057B)]"></div>
+       <div className="h-[150px] mb-[0px] w-full  bg-[linear-gradient(to_bottom,transparent,transparent,#52057B)]"></div>
      </div>
    );
  };
